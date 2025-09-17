f21f2016b9bdb8ff4fcad12e665dc0d1e2118f1e
edward795 <ayyappadasgb777999@gmail.com>
2025-09-17 09:54:02 +0530

    Java 9 : HttpClient Introduction

---
+++ src/main/java/org/example/CallingRestEndpointsExample.java
@@ -0,0 +1,53 @@
+package org.example;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class CallingRestEndpointsExample {
+    public static void main(String[] args) throws IOException, InterruptedException {
+        String endpoint="https://jsonplaceholder.typicode.com/posts";
+        HttpURLConnection connection=null;
+        try{
+            URL url=new URL(endpoint);
+            connection=(HttpURLConnection) url.openConnection();
+            connection.setRequestMethod("GET");
+            connection.setConnectTimeout(5000);
+            connection.setReadTimeout(5000);
+
+            int status=connection.getResponseCode();
+            System.out.println("Status : "+status);
+
+            try(BufferedReader reader=new BufferedReader(
+                    new InputStreamReader(connection.getInputStream())
+            )){
+                while((reader.readLine()!=null)){
+                    System.out.println(reader.readLine());
+                }
+            }
+        }catch (IOException e){
+            e.printStackTrace();
+        }finally {
+            if(connection!=null) connection.disconnect();
+        }
+
+
+        HttpClient client=HttpClient.newHttpClient();
+        HttpRequest HttpRequest= java.net.http.HttpRequest.newBuilder()
+                .uri(URI.create(endpoint))
+                .GET()
+                .build();
+        HttpResponse<String> response=client.send(HttpRequest,
+                HttpResponse.BodyHandlers.ofString());
+        System.out.println("Status : "+response.statusCode());
+        System.out.println(response.body());
+
+    }
+}
