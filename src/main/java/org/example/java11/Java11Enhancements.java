7ab270739aa4c9e0b1ddd7f1f6eb5c9cbd76b933
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-09-27 12:48:21 +0530

    java 11 : enhancements

--- src/main/java/org/example/java11/Java11Enhancements.java
+++ src/main/java/org/example/java11/Java11Enhancements.java
@@ -1,13 +1,17 @@
 package org.example.java11;
 
 import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.Optional;
 import java.util.function.BiFunction;
 
 public class Java11Enhancements {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, InterruptedException {
 
         //var keyword enhancement in lambda
 
@@ -20,13 +24,15 @@ public class Java11Enhancements {
 
         //Stream api improvements
         String text="  Hello,World  \n" +
-                "  My first Program in Java ";
-        System.out.println(text.isBlank());
-        System.out.println(text.strip());
-        System.out.println("Hi!".repeat(3));
+                "  This is my first Program in Java \n"
+                +"  Java is a great language.";
 
-        System.out.println();
+        System.out.println("is the text blank ? :"+text.isBlank());
+        System.out.println("extracting the lines : ");
         text.lines().forEach(System.out::println);
+        System.out.println(text.strip());
+        System.out.println(text.stripIndent());
+        System.out.println(text.stripTrailing());
 
         //Optional Enhancements
         Optional<String> sampleTxt=Optional.of(" ");
@@ -38,5 +44,37 @@ public class Java11Enhancements {
         Files.writeString(path,"Hello Java 11");
         String content=Files.readString(path);
         System.out.println(content);
+
+        //Paths.get() --> Paths.of() enhancement
+        //single path
+        Path path1=Path.of("folder/file.txt");
+        System.out.println(path1);
+
+        //multiple parts of a path
+        //relative path
+        Path path2=Path.of("folder","subfolder","file.txt");
+        System.out.println(path2);
+
+        //absolute path
+        Path path3=Path.of("/Users/ayyappadas/Documents/test.txt");
+        System.out.println(path3);
+
+        //URI support
+        URI uri=URI.create("file:///Users/ayyappadas/text.txt");
+        Path path4=Path.of(uri);
+        System.out.println(uri);
+
+        //New HTTP client API(standard)
+        HttpClient client=HttpClient.newHttpClient();
+        HttpRequest request=HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .build();
+        HttpResponse<String> response=client.send(request,HttpResponse.BodyHandlers.ofString());
+        System.out.println(response.body());
+
+        //Running Single source file
+        //you can now run single source files in java without compiling first
+        //java HelloWorld.java
+
     }
 }
