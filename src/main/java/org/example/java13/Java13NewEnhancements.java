db260a957419ef9a1d37313524611e9fcc8fd439
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-09-30 10:58:10 +0530

    java 13 enhancements

---
+++ src/main/java/org/example/java13/Java13NewEnhancements.java
@@ -0,0 +1,16 @@
+package org.example.java13;
+
+public class Java13NewEnhancements {
+    public static void main(String[] args) {
+        //multiline text blocks
+        String html= """
+                <html>  
+                    <body>
+                        <h1>Hello, World!</h1> 
+                    </body>
+                </html>
+                """;
+
+        System.out.println(html);
+    }
+}
