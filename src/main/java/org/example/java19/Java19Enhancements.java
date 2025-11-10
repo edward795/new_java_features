9393f83fbc4609f8587f4f03aa6a441b64f36220
ayyappadas-gb <ayyappadas.gb@ibm.com>
2025-11-10 12:42:54 +0530

    java 19 enhancements

--- src/main/java/org/example/java19/Java19Enhancements.java
+++ src/main/java/org/example/java19/Java19Enhancements.java
@@ -1,19 +1,19 @@
 package org.example.java19;
 
-import java.lang.foreign.Arena;
-import java.lang.foreign.MemorySegment;
+import java.lang.foreign.*;
+import java.lang.invoke.MethodHandle;
 import java.util.concurrent.*;
 
-import static java.lang.foreign.ValueLayout.JAVA_INT;
+import static java.lang.foreign.ValueLayout.*;
 
 public class Java19Enhancements {
     public static void main(String[] args) {
         //virtual threads
-        try(ExecutorService executor= Executors.newVirtualThreadPerTaskExecutor()){
-            for(int i=0;i<10;i++){
-                int taskId=i;
-                executor.submit(()->{
-                    System.out.println("Running task "+taskId+" on "+Thread.currentThread());
+        try (ExecutorService executor = Executors.newVirtualThreadPerTaskExecutor()) {
+            for (int i = 0; i < 10; i++) {
+                int taskId = i;
+                executor.submit(() -> {
+                    System.out.println("Running task " + taskId + " on " + Thread.currentThread());
                     Thread.sleep(1000);
                     return taskId;
                 });
@@ -21,13 +21,13 @@ public class Java19Enhancements {
         }
 
         //structured concurrency
-        try(var scope= new StructuredTaskScope.ShutdownOnFailure()){
-            Future<String> user=scope.fork(()->fetchUserData()).get();
-            Future<String> orders=scope.fork(()->fetchUserData()).get();
+        try (var scope = new StructuredTaskScope.ShutdownOnFailure()) {
+            Future<String> user = scope.fork(() -> fetchUserData()).get();
+            Future<String> orders = scope.fork(() -> fetchUserData()).get();
 
             scope.join();
             scope.throwIfFailed();
-            System.out.println(user.resultNow()+" "+orders.resultNow());
+            System.out.println(user.resultNow() + " " + orders.resultNow());
         } catch (ExecutionException e) {
             throw new RuntimeException(e);
         } catch (InterruptedException e) {
@@ -35,14 +35,30 @@ public class Java19Enhancements {
         }
 
         //Foreign Memory API
-        try(Arena arena=Arena.ofConfined()){
-            MemorySegment memorySegment=arena.allocate(JAVA_INT);
-            memorySegment.set(JAVA_INT,0,42);
-            System.out.println(memorySegment.get(JAVA_INT,0));
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment memorySegment = arena.allocate(JAVA_INT);
+            memorySegment.set(JAVA_INT, 0, 42);
+            System.out.println(memorySegment.get(JAVA_INT, 0));
+        }
+
+
+        Linker linker = Linker.nativeLinker();
+        SymbolLookup lookup = linker.defaultLookup();
+
+        MethodHandle strlen = linker.downcallHandle(
+                lookup.find("strlen").get(),
+                FunctionDescriptor.of(JAVA_LONG, ADDRESS));
+
+        try (Arena arena = Arena.ofConfined()) {
+            MemorySegment str = arena.allocateUtf8String("Hello");
+            long len = (long) strlen.invoke(str);
+            System.out.println("String Length : " + len);
+        } catch (Throwable e) {
+            throw new RuntimeException(e);
         }
     }
 
-    static Future<String> fetchUserData(){
-            return CompletableFuture.failedFuture(new Throwable("error"));
+    static Future<String> fetchUserData() {
+        return CompletableFuture.failedFuture(new Throwable("error"));
     }
 }
